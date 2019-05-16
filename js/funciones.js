54053efa15c4b7b884c2f1ee03f18da8cd15f8e1
kingarkal <50721113+kingarkal@users.noreply.github.com>
2019-05-17 00:26:20 +0200

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,15 @@
+function registrado(){
+  alert("registrado con exito");
+}
+
+function conectado(){
+  alert("conectado con exito");
+}
+
+function consulta(){
+  alert("ya has realizado la consulta");
+}
+
+function peticion(){
+  alert("ya has realizado la peticioin");
+}
